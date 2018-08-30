7ebfc4c99ee4bb85a8e073113d20dcf6ffd0ae53
Jack Cloudman <maylo360xd@gmail.com>
2018-08-30 01:59:28 -0500

    Busqueda actualizada

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -3,9 +3,10 @@ from django.views.generic.base import RedirectView
 from django.urls import path
 from . import views
 urlpatterns = [
-    url(r'^$', views.home),
+    url(r'^$', views.post_list),
     url(r'^page/$', RedirectView.as_view(url='/', permanent=False)),
-    url(r'^page/(?P<page>[0-9]+)/$', views.home, name='page_list'),
+    url(r'^search/$', views.search_list, name='post_list'),
+    url(r'^page/(?P<page>[0-9]+)/$', views.post_list, name='post_list'),
     url(r'^post/(?P<pk>[0-9]+)/(?P<title>[-a-zA-Z0-9_]+)/$', views.post_detail, name='post_detail'),
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
 ]
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,20 +1,52 @@
 from django.shortcuts import render,get_object_or_404,redirect
 from .models import Post
 from django.utils import timezone
-def home(request,page=0):
+from django.db.models import Q
+def post_list(request,page=0):
     page = int(page)
+    posts = Post.objects.filter(fecha_publicado__lte=timezone.now()).exclude(slug__exact=None).order_by('-fecha_publicado')
+    total = posts.count()
     if page==0:
-        posts = Post.objects.filter(fecha_publicado__lte=timezone.now()).exclude(slug__exact=None).order_by('-fecha_publicado')[:4]
+        posts = posts[:4]
     else:
-        posts = Post.objects.filter(fecha_publicado__lte=timezone.now()).exclude(slug__exact=None).order_by('-fecha_publicado')[page*4:page*4+4]
-    
-    total = len(Post.objects.filter(fecha_publicado__lte=timezone.now()))
-    post_antiguos = (page*4+4) <=total
+        posts = posts[page*4:page*4+4]
+
+    post_antiguos = (page*4+4) <total
     post_recientes = page>=1
     anterior = page-1
     siguiente = page+1
     return render(request, 'index.html', {'posts':posts,'post_antiguos':post_antiguos,'post_recientes':post_recientes,
                         'anterior':anterior,'siguiente':siguiente})
+def search_list(request):
+    page = request.GET.get("page")
+    query = request.GET.get("q")
+    if not query:
+        return redirect("/")
+    posts = Post.objects.filter(fecha_publicado__lte=timezone.now()).exclude(slug__exact=None)
+    posts = posts.filter(
+                        Q(titulo__icontains=query) | 
+                        Q(descripcion__icontains=query) |
+                        Q(tags__icontains=query) | 
+                        Q(contenido__icontains=query) |
+                        Q(autor__first_name__icontains=query) |
+                        Q(autor__last_name__icontains=query)).distinct().order_by('-fecha_publicado')
+    total = posts.count()
+    if page:
+        page = int(page)-1
+        posts = posts[page*4:page*4+4]
+    else:
+        page=0
+        posts = posts[:4]
+    post_antiguos = (page*4+4) <total
+    post_recientes = page>=1
+    anterior = page-1
+    siguiente = page+1
+    contexto = {'posts':posts,
+                'post_antiguos':post_antiguos,
+                'post_recientes':post_recientes,
+                'anterior':anterior+1,
+                'siguiente':siguiente+1}
+    return render(request, 'search.html',contexto)
 def post_detail(request, pk,title=None):
     post = Post.objects.filter(fecha_publicado__lte=timezone.now(),pk=pk).exclude(slug__exact=None)
     if not post:get_object_or_404(Post,pk=pk)
